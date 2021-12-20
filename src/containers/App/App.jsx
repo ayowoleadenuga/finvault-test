d8ddb9a9ea21d8bcd8ebc6da7891e910681855e5
Ayowole <ayowoleadenuga@gmail.com>
2021-12-20 08:16:57 +0100

    Update App.jsx

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,25 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../../scss/app.scss';
-import Router from './Router';
-import store from './store';
-import ScrollToTop from './ScrollToTop';
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../scss/app.scss";
+import Router from "./Router";
+import ScrollToTop from "./ScrollToTop";
 
-const App = () =>  (
-  <Provider store={store}>
-    <BrowserRouter>
-      <ScrollToTop>
-        <Fragment>
-          <div>
-            <Router />
-          </div>
-        </Fragment>
-      </ScrollToTop>
-    </BrowserRouter>
-  </Provider>
+const App = () => (
+  <BrowserRouter>
+    <ScrollToTop>
+      <Fragment>
+        <div>
+          <Router />
+        </div>
+      </Fragment>
+    </ScrollToTop>
+  </BrowserRouter>
 );
 
 export default App;
